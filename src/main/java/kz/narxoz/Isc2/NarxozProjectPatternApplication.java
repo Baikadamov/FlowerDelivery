c5008701fe5e302e6f9bd6683e640abbaa594170
Baikadamov <maxat.baikadamov@narxoz.kz>
2022-11-12 14:40:30 +0600

    init 1

---
+++ src/main/java/kz/narxoz/Isc2/NarxozProjectPatternApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.Isc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarxozProjectPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarxozProjectPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/narxoz/Isc2/NarxozProjectPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.Isc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarxozProjectPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
